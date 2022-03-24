8719e51d6bfac880c160a238d6d4065471c41ae6
Joshu Strassle <j.strassle@icloud.com>
2022-03-24 14:33:34 -0400

    show hide buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
+    const [intro, setIntro] = useState<boolean>(true);
+    const [use, setUse] = useState<boolean>(true);
+    const [hooks, setHooks] = useState<boolean>(true);
+    const [examples, setExamples] = useState<boolean>(true);
     return (
         <div className="App">
             <header className="App-header">
@@ -11,6 +16,34 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <Button
+                onClick={() => {
+                    setIntro(!intro);
+                }}
+            >
+                Introduction
+            </Button>
+            <Button
+                onClick={() => {
+                    setUse(!use);
+                }}
+            >
+                useState
+            </Button>
+            <Button
+                onClick={() => {
+                    setHooks(!hooks);
+                }}
+            >
+                Hooks
+            </Button>
+            <Button
+                onClick={() => {
+                    setExamples(!examples);
+                }}
+            >
+                Examples
+            </Button>
         </div>
     );
 }
